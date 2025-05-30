3707ede0154b0245c935945ec9488adf1b1ec905
SHREYANSH726 <lshreyansh726@gmail.com>
2025-05-30 11:46:21 +0530

    Add files via upload

---
+++ Navigator  router  search/src/Component/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder='USERNAME'/>
+        <input type="text" placeholder='PASSWORD' />
+        
+        <button>SUBMIT</button>
+      </form>
+      
+    </div>
+  )
+}
+
+export default login
